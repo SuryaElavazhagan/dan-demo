c12badb8ab649eb5cc5ed676ad13a59865f72626
Surya Elavazhagan <kingslayer@Suryas-MacBook-Air.local>
2018-10-31 17:55:48 +0530

    Updates

---
+++ precache-manifest.0b67b8e411c5b9f564ad901549c02652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27b3841c29aa48c107a7",
+    "url": "/dan-demo/static/js/runtime~main.27b3841c.js"
+  },
+  {
+    "revision": "ca226f3e665ade8928f4",
+    "url": "/dan-demo/static/js/main.ca226f3e.chunk.js"
+  },
+  {
+    "revision": "3c6fd63e4cd156ba73ef",
+    "url": "/dan-demo/static/js/1.3c6fd63e.chunk.js"
+  },
+  {
+    "revision": "ca226f3e665ade8928f4",
+    "url": "/dan-demo/static/css/main.3c2e609c.chunk.css"
+  },
+  {
+    "revision": "95749c41d584bb87606c3b90154c390e",
+    "url": "/dan-demo/index.html"
+  }
+];
